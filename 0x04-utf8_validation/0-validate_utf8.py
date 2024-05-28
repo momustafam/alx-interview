0602e144268922d5a8c4f2e5ad63a7583350d461
Mohammed Mustafa <88572251+momustafam@users.noreply.github.com>
2024-05-28 07:32:22 +0300

    Create 0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+
+#!/usr/bin/python3
+"""UTF-8 Validation Module"""
+
+
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding."""
+    count = 0
+
+    for bit in data:
+        binary = bin(bit).replace('0b', '').rjust(8, '0')[-8:]
+        if count == 0:
+            if binary.startswith('110'):
+                count = 1
+            if binary.startswith('1110'):
+                count = 2
+            if binary.startswith('11110'):
+                count = 3
+            if binary.startswith('10'):
+                return False
+        else:
+            if not binary.startswith('10'):
+                return False
+            count -= 1
+
+    if count != 0:
+        return False
+
+    return True
